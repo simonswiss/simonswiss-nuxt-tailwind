2f6beb966b5eb41bb81bbcc2cb1369765b430f07
Simon Vrachliotis <simon@simonswiss.com>
2018-04-02 21:34:44 +1000

    fixing broken footer flexbox on main layout with sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="flex flex-col h-screen justify-between">
-    <div class="my-16 max-w-xl mx-auto px-6 md:flex font-sans leading-normal">
-      <aside class="md:w-1/2 lg:w-1/3">
-        <SideBar/>
-      </aside>
-      <main class="md:w-1/2 lg:w-2/3 md:pl-16 max-w-md">
-        <nuxt/>
-      </main>
+    <div>
+      <div class="my-16 max-w-xl mx-auto px-6 md:flex font-sans leading-normal">
+        <aside class="md:w-1/2 lg:w-1/3">
+          <SideBar/>
+        </aside>
+        <main class="md:w-1/2 lg:w-2/3 md:pl-16 max-w-md">
+          <nuxt/>
+        </main>
+      </div>
     </div>
     <Footer/>
     <script src="https://embed.small.chat/T5F7UGFT3G5G0GCT3P.js" async></script>
