e1825892f8a695b16605b250bf669f903b8c9f8f
Simon Vrachliotis <simon@simonswiss.com>
2018-04-03 21:34:15 +1000

    extracting sidebar components into their own file

---
+++ components/sidebar/Nav.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul class="list-reset mb-6">
+      
+      <li class="mb-1">
+        <nuxt-link class="text-purple no-underline hover:underline" to="/">About</nuxt-link>
+      </li>
+
+      <li class="mb-1">
+        <nuxt-link class="text-purple no-underline hover:underline" to="/blog">Blog</nuxt-link>
+      </li>
+
+      <li class="mb-1">
+        <nuxt-link class="text-purple no-underline hover:underline" to="/contact">Contact</nuxt-link>
+      </li>
+    
+    </ul>
+</template>
+
+<script>
+export default {};
+</script>
